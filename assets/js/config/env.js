9a24bfcd587622cfb505656d7f0fa3944534a01e
rkota <rosairekota@gmail.com>
2022-03-26 20:26:31 +0100

    refactor: update config/env.js

--- assets/js/config/env.js
+++ assets/js/config/env.js
@@ -1,1 +1,7 @@
-export const baseUrl = "https://127.0.0.1:8000/api/image/get-images";
+import axios from "axios";
+
+export const baseUrl = "https://127.0.0.1:8000/api";
+
+export const baseApi = axios.create({
+  baseURL: baseUrl,
+});
